a66ae72703ac321674c3fdb27d09fa28925191cc
careck <careck@gmail.com>
2014-11-29 10:57:51 +1100

    config setup

--- tribalfire/config.lua
+++ tribalfire/config.lua
@@ -1,29 +1,145 @@
-application = {
-	content = {
-		width = 320,
-		height = 480, 
-		scale = "letterBox",
-		fps = 30,
-		
-		--[[
-        imageSuffix = {
-		    ["@2x"] = 2,
-		}
-		--]]
-	},
-
-    --[[
-    -- Push notifications
-
-    notification =
+if string.sub(system.getInfo("model"),1,4) == "iPad" then
+    application = 
     {
-        iphone =
-        {
-            types =
+        content =
+        {           
+            fps = 60,
+            width = 360,
+            height = 480,
+            scale = "letterBox",
+            xAlign = "center",
+            yAlign = "center",
+            imageSuffix = 
             {
-                "badge", "sound", "alert", "newsstand"
-            }
+                ["@2x"] = 1.5,
+                ["@4x"] = 3.0,
+            },
+        },
+        notification = 
+        {
+            iphone = {
+                types = {
+                    "badge", "sound", "alert"
+                }
+            },
+            google =
+        {
+                projectNumber = "xxxx",
+         },
         }
     }
-    --]]    
-}
+
+elseif string.sub(system.getInfo("model"),1,2) == "iP" and display.pixelHeight > 960 then
+    application = 
+    {
+        content =
+        {
+            antialias = true,
+            fps = 60,
+            width = 320,
+            height = 568,
+            scale = "letterBox",
+            xAlign = "center",
+            yAlign = "center",
+            imageSuffix = 
+            {
+                ["@2x"] = 1.5,
+                ["@4x"] = 3.0
+            },
+        },
+        notification = 
+        {
+            iphone = {
+                types = {
+                    "badge", "sound", "alert"
+                }
+            },
+            google =
+        {
+                projectNumber = "xxxx",
+         },
+        }
+    }
+
+elseif string.sub(system.getInfo("model"),1,2) == "iP" then
+    application = 
+    {
+        content =
+        {
+            antialias = true,
+            fps = 60,
+            width = 320,
+            height = 480,
+            scale = "letterBox",
+            xAlign = "center",
+            yAlign = "center",
+            imageSuffix = 
+            {
+                ["@2x"] = 1.5,
+                ["@4x"] = 3.0,
+            },
+        },
+        notification = 
+        {
+            iphone = {
+                types = {
+                    "badge", "sound", "alert"
+                }
+            },
+            google =
+        {
+                projectNumber = "xxxx",
+         },
+        }
+    }
+elseif display.pixelHeight / display.pixelWidth > 1.72 then
+    application = 
+    {
+        content =
+        {
+            antialias = true,
+            fps = 60,
+            width = 320,
+            height = 570,
+            scale = "letterBox",
+            xAlign = "center",
+            yAlign = "center",
+            imageSuffix = 
+            {
+                ["@2x"] = 1.5,
+                ["@4x"] = 3.0,
+            },
+        },
+    }
+else
+    application = 
+    {
+        content =
+        {
+            antialias = true,
+            fps = 60,
+            width = 320,
+            height = 512,
+            scale = "letterBox",
+            xAlign = "center",
+            yAlign = "center",
+            imageSuffix = 
+            {
+                ["@2x"] = 1.5,
+                ["@4x"] = 3.0,
+            },
+        },
+        notification = 
+        {
+            iphone = {
+                types = {
+                    "badge", "sound", "alert"
+                }
+            },
+            google =
+        {
+                projectNumber = "xxxx",
+         },
+        }
+    }
+end
