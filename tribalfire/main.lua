fd95a3412c0316b69ef483bd43f532bd11a18a6e
careck <careck@gmail.com>
2014-11-29 11:04:52 +1100

    added screen dimensions

--- tribalfire/main.lua
+++ tribalfire/main.lua
@@ -5,6 +5,14 @@
 -----------------------------------------------------------------------------------------
 
 -- Your code here
+
+
+local screenHeight = display.viewableContentHeight
+local screenWidth = display.viewableContentWidth
+
+image.x = screenWidth / 2
+image.y = screenHeight / 2
+
 display.setStatusBar(display.HiddenStatusBar)
 
 local backgrounds = require(backgrounds)
