4bb81c4e148992b8b8eb606965c2b86dde5cb1aa
liamod <liamod@soccerfit.com.au>
2014-11-30 13:19:24 +1100

    reset

--- tribalfire/gameover.lua
+++ tribalfire/gameover.lua
@@ -2,8 +2,10 @@
 function gameOver()
   gameStatus = "GameOver"
   -- adds image centered at x,y co-ords
-  local gameoverpic = display.newImage("images/gameover2.png", display.contentCenterX, display.contentCenterY)
+  gameoverpic = display.newImage("images/gameover2.png", display.contentCenterX, display.contentCenterY)
   audio.stop(backgroundMusicChannel)
-  audio.play(gameoverSong)
+  theGameoverSongChannel = audio.play(gameoverSong)
+  --print( "gameoverpic is ")
+  --print( gameoverpic )
 end
 
