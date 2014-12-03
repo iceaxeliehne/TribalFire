f8bdc48c23287a69d8b678479fe1ec8b38bbe526
iceaxeliehne <iceaxe.liehne@gmail.com>
2014-12-03 15:08:25 +1100

    attempted changes to tile handler asset tab improved slightly no more floating platforms for now

--- tribalfire/assets.lua
+++ tribalfire/assets.lua
@@ -23,95 +23,29 @@ level ={
 		}
 	}
 }
-collisions = {}
+
+-- holds functions that allow character to anticipate actions before colliding with the object. indexed by tile collision index
 preCollisions = {}
+-- holds function for side on collisions with tile
+collisions = {}
+-- holds functions called when the character is above the tile
+aboveCollision = {}
+-- holds functions called when character is below the tile
+belowCollision ={}
 
 -- pre-collision function for tile 1
 preCollisions[1] =  function (t)
-	print('pre-collision')
-	print(player.y)
-	print(ground)
-	print('t.y = '..t.y)
 	if player.y == ground then
-		if t.y <= (player.y+ 100) and t.y >= (player.y-50) then
-			print('test second jump')
-			player.state = 'jumpUp'
-			if t.y <= screenHeight - 100 then
-				t.testForCollision = 1
-			end
-		else
-			t.testForCollision = 1
-		end
-	else
-		t.testForCollision = 1
+		player.state = 'jumpUp'
 	end
 end
-
-function preCollision(t)
-	--t.testForCollision = 1
-	print('test pre collision')
-	print(t.collisionIndex)
-	--local index = t.collisionIndex
-	preCollisions[t.collisionIndex](t)
-end
-
-
 
 -- collision function for tile 1
-collisions[1] = function (t,upOrDown)
-	local above = player.y >= t.y and player.x >= (t.x - 40) and player.x <= (t.x + 40)
-	local side = t.x <= 132 and player.y >= (t.y - 20) and player.y <= (t.y + 20)
-	local below = player.y <= t.y and player.x >= (t.x - 40) and player.x <= (t.x + 40)
-	if side then
-		if player.y < t.y then
-			if (t.y - player.y) >= compareValues(t.x,player.x) then
-				ground = t.y - 52
-				print('set ground to '..ground)
-				groundTile = t
-			else
-				print('side from above')
-				gameOver()
-			end
-		elseif t.y < player.y  then
-			if (player.y - t.y) >= compareValues(t.x,player.x) then
-				player.state = 'jumpingDown'
-			else
-				print('side from below')
-				gameOver()
-			end
-		else
-			print('side')
-			gameOver()
-		end
-	elseif above then
-		ground = t.y - 52
-		print('set ground to '..ground)
-		groundTile = t	
-	elseif below then
-		player.state = 'jumpingDown'
-	end
+collisions[1] = function (t)
+	print("collision test")
+	--gameOver()
 end
 
-function compareValues(a,b)
-	if a >=b then
-		return a - b
-	else
-		return b - a
-	end
-end
-	
-
-function collisionTest(t)
-	if t.x <= 48 then
-		t.testForCollision = 0
-		return
-	end
-	local side = 132 >= t.contentBounds.xMin 
-	local up = player.contentBounds.yMax >= t.contentBounds.yMin and player.contentBounds.yMin <= t.contentBounds.yMin
-	local down = player.contentBounds.yMin <= t.contentBounds.yMax and player.contentBounds.yMin >= t.contentBounds.yMin
-	if (side and down) then
-		collisions[t.collisionIndex](t,'down')
-	elseif (side and up) then
-		collisions[t.collisionIndex](t,'up')
-	end
+aboveCollision[1] = function (t)
+	ground = ground - 40
 end
--- tribalfire/tile2.lua
+++ tribalfire/tile2.lua
@@ -2,17 +2,23 @@
 local screenHeight = display.viewableContentHeight
 local screenWidth = display.viewableContentWidth
 
-
+-- initiate tile for display
 function displayObjectInit(params)
 	local t = display.newImageRect(params.image,40,40)
-	t.x = params.x
-	t.y = params.y
+	t.x = -25
+	t.y = 35
+	-- state can be 'slider','static', 'scrolling', 'falling'
 	t.state = params.state
+	-- collision index sends to unique functions for tile collisions
 	t.collisionIndex = params.collisionIndex
 	--change to 0 to disable touch
 	t.touchState = 1
 	t:addEventListener( "touch", tileTouch )
+	-- pre collision state checks wether the character has reacted to the approaching tile
+	t.preCollisionState = 0
+	-- collision state checks if colision function has run. 0 = can collide, 1 = has collided
 	t.collisionState = 0
+	-- testForCollision if 1 will check for collision with player
 	t.testForCollision = 0
 	tiles:insert(t)
 end
@@ -31,9 +37,16 @@ function updateTiles()
 			end
 		elseif tiles[i].state =='scrolling' then
 			tiles[i].x = tiles[i].x -1
-			if tiles[i].x <= 150 and tiles[i].collisionState == 0 then
-				tiles[i].collisionState = 1
-				collisionHandler(tiles[i])
+			if tiles[i].x <= 155 and tiles[i].collisionState == 0 then
+				tiles[i].preCollisionState = 1
+				tiles[i].testForCollision = 1
+				preCollisions[tiles[i].collisionIndex](tiles[i])
+			end
+		elseif tiles[i].state == 'falling' then
+			tiles[i].y = tiles[i].y +3
+			if tiles[i].y >= ground + 12 then
+				tiles[i].y = ground + 12
+				tiles[i].state = 'scrolling'
 			end
 		end
 		if tiles[i].testForCollision == 1 then
@@ -58,26 +71,54 @@ function tileTouch(event)
 		if event.phase == "began" then
 			print('hit')
 		elseif event.phase == "moved" then
-			event.target.x = event.x
-			event.target.y = event.y
+			if event.x <= 200 then
+				event.target.x = 200
+			else
+				event.target.x = event.x
+			end
+			if event.y >= screenHeight - 50 then
+				event.target.y = ground
+			else
+				event.target.y = event.y
+			end
 		elseif event.phase == "ended" then
 			event.target.touchState = 0
-			event.target.state = 'scrolling'
+			if event.target.y <= ground then
+				event.target.state = "falling"
+			else
+				event.target.state = 'scrolling'
+			end
 			--tilesInHand = tilesInHand - 1
 			updateTetris()
 		end
 	end
 end
 
-function collisionHandler(t)
-	preCollision(t)
-end
 
 function updateTetris()
 	for i=1, tiles.numChildren, 1 do
 		if tiles[i].state == 'static' then
 			tiles[i].state = 'slider'
 			tilesInHand = 1
+		end
+	end
+end
+
+function collisionTest(t)
+	if t.x <= 48 then
+		t.testForCollision = 0
+		return
+	elseif t.contentBounds.xMin <= player.contentBounds.xMax then
+		if t.contentBounds.yMin > player.contentBounds.yMax then
+			--player is above tile
+			aboveCollision[t.collisionState](t)
+		elseif t.contentBounds.yMax < player.contentBounds.yMin then
+			--player is below tile
+			--belowCollision[t.collisionState](t)
+		else
+			-- player is colliding from the side
+			t.collisionState = 1
+			collisions[t.collisionIndex](t)
 		end
 	end
 end
